d7601a6b4a64ad243e1ef71382b32ce53f1377d9
Hannu Lyytikainen <hannu@sharetribe.com>
2019-12-04 13:19:45 +0200

    Export file token store from the sdk entry file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import SharetribeSdk from './integration_sdk';
 import * as types from './types';
 import memoryStore from './memory_store';
+import fileStore from './file_store';
 
 const createInstance = config => new SharetribeSdk(config);
 
 // Export token stores
 const tokenStore = {
   memoryStore,
+  fileStore,
 };
 
 /* eslint-disable import/prefer-default-export */
