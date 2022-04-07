b795dea7700619ba9ffa45a1c517d1b3cb85be59
WON SEOB SEO <pghoou@gmail.com>
2022-04-07 10:38:48 +0300

    Add listings create endpoint

--- build/sharetribe-flex-integration-sdk-node.js
+++ build/sharetribe-flex-integration-sdk-node.js
@@ -3572,7 +3572,14 @@ var endpointDefinitions = [{
   internal: false,
   method: 'post',
   interceptors: [new __WEBPACK_IMPORTED_MODULE_20__interceptors_transit_response__["a" /* default */](), new __WEBPACK_IMPORTED_MODULE_19__interceptors_transit_request__["a" /* default */]()]
-}, {
+  },
+{
+  apiName: 'integration_api',
+  path: 'listings/create',
+  internal: false,
+  method: 'post',
+  interceptors: [new __WEBPACK_IMPORTED_MODULE_20__interceptors_transit_response__["a" /* default */](), new __WEBPACK_IMPORTED_MODULE_19__interceptors_transit_request__["a" /* default */]()]
+},  {
   apiName: 'integration_api',
   path: 'listings/close',
   internal: false,
